25fe587e49df19a68b0e98523cfcc5bad26290cd
shobhit saini <shobhitsaini709@gmail.com>
2023-05-16 06:45:56 +0530

    Integrated stripe payment

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,23 +1,82 @@
+import React, { useState } from "react";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useSelector } from "react-redux";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
-import { FormContainer, PaymentFormContainer } from "./payment-form.style";
+import {
+  FormContainer,
+  PaymentFormContainer,
+  PaymentButton,
+  Spinner,
+} from "./payment-form.style";
+
+import { selectCartTotal } from "../../store/cart/cart.selector";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const amount = useSelector(selectCartTotal);
+  const user = useSelector(selectCurrentUser);
+  const [isProcessing, setIsProcessing] = useState(false);
 
   const stripePaymentHandler = async (e) => {
     e.preventDefault();
 
     if (!stripe || !elements) return;
+
+    setIsProcessing(true);
+
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: amount * 100 }),
+    }).then((res) => res.json());
+
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: user ? user.displayName : "Guest",
+          address: {
+            line1: "510 Townsend St",
+            postal_code: "98140",
+            city: "San Francisco",
+            state: "CA",
+            country: "US",
+          },
+        },
+      },
+    });
+
+    setIsProcessing(false);
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment successful");
+      }
+    }
   };
 
   return (
-    <PaymentFormContainer onSubmit={stripePaymentHandler}>
-      <FormContainer>
+    <PaymentFormContainer>
+      <FormContainer onSubmit={stripePaymentHandler}>
+        <h2>Credit card payment:</h2>
         <CardElement />
-        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
+        <PaymentButton
+          isLoading={isProcessing}
+          buttonType={BUTTON_TYPE_CLASSES.inverted}
+        >
+          {isProcessing ? <Spinner /> : "Pay now"}
+        </PaymentButton>
       </FormContainer>
     </PaymentFormContainer>
   );
--- src/components/payment-form/payment-form.style.jsx
+++ src/components/payment-form/payment-form.style.jsx
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+import { SpinnerContainer } from "../spinner/spinner.styles";
+
+import { BaseButton } from "../button/button.styles";
+
 export const PaymentFormContainer = styled.div`
   height: 300px;
   display: flex;
@@ -11,4 +15,14 @@ export const PaymentFormContainer = styled.div`
 export const FormContainer = styled.form`
   height: 100px;
   min-width: 500px;
+`;
+
+export const Spinner = styled(SpinnerContainer)`
+  width: 30px;
+  height: 30px;
+`;
+export const PaymentButton = styled(BaseButton)`
+  margin-left: auto;
+  margin-top: 30px;
+  align-items: center;
 `;
