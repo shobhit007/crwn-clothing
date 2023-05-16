2458ce883d3ef337b9fe35a14ce6f1a48219b77c
shobhit saini <shobhitsaini709@gmail.com>
2023-05-16 07:29:14 +0530

    some ui changes

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import { useSelector } from "react-redux";
 
-import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import {
   FormContainer,
   PaymentFormContainer,
